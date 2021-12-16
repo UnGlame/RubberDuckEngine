905b11daae7062dd7877976510cf7af57c3c10a7
Joel <35512452+UnGlame@users.noreply.github.com>
2021-12-16 19:48:40 +0800

    Initial setup of project

---
+++ RubberDuckEngine/RubberDuckEngine/Source/Pch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <cstdlib>
+#include <memory>
+#include <cassert>
+#include <vector>
+#include <utility>
+#include <algorithm>
